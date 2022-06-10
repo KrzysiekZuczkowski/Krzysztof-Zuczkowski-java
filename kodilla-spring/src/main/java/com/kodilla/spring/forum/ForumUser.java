8b87185cd8e7f9bbc537b61eea9e4c4991b417e1
Krzysiek Zuczkowski <krzysiek23@windowslive.com>
2022-06-10 22:30:16 +0200

    14.1 bean ForumUser

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,17 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    private String username;
+
+    public ForumUser() {
+        username = "John Smith";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
