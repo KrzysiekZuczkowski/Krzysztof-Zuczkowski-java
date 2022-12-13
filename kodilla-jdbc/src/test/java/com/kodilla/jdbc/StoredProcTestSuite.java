c1e7bb1f07ef7fb7b980595c788bcf0365f2ac62
Krzysiek Zuczkowski <krzysiek23@windowslive.com>
2022-12-13 19:48:35 +0100

    30.3 storedProcTestSuite

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,75 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StoredProcTestSuite {
+
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        int howManyBefore = -1;
+        if (rs.next()) {
+            howManyBefore = rs.getInt("HOW_MANY");
+        }
+
+        // When
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+
+        // Then
+        rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(5, howManyBefore);
+        assertEquals(0, howMany);
+        rs.close();
+        statement.close();
+    }
+
+    @Test
+    void testUpdateBestsellers() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = FALSE";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER = TRUE";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        int howManyBefore = -1;
+        if (rs.next()) {
+            howManyBefore = rs.getInt("HOW_MANY");
+        }
+
+        // When
+        //Procedura zmieniona teraz identyfikuje bestsellery czyli ksiązki wypożyczone więcej niż raz w miesiącu
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        // Then
+        rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0, howManyBefore);
+        assertEquals(2, howMany);
+        rs.close();
+        statement.close();
+    }
+}
