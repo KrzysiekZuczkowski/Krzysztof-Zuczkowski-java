0ec98b34a12674e8bbe19d0dc53a74672e81db67
Krzysiek Zuczkowski <krzysiek23@windowslive.com>
2022-12-15 23:26:00 +0100

    31.1 selenium webdriver ebay

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,6 +1,23 @@
 package com.kodilla.testing2.ebay;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class EBayTestingApp {
 
+    public static final String SEARCHFIELD = "_nkw";
 
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.ebay.com");
+
+//        driver.findElement(By.xpath("//*[@id=\"gdpr-banner-accept\"]/div")).click();
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
 }
